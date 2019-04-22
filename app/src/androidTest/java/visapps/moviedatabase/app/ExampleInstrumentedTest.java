2ef6fe439bb71ee1e0ff5a0dc038b257cf59395f
Vasily Materikin <visek@MBP-Vasily.lan>
2019-04-22 16:03:22 +0300

    Fixed instrumented test

--- app/src/androidTest/java/visapps/moviedatabase/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/visapps/moviedatabase/app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.visapps.cinemaonline", appContext.getPackageName());
+        assertEquals("visapps.moviedatabase.app", appContext.getPackageName());
     }
 }
